22799535214d2e888c6a39c048f77e3c85c88d34
MD-Enriquez <marc.dominic.enriquez@eee.upd.edu.ph>
2022-07-25 12:42:50 +0800

    second commit

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const http = require('http')
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.end('Welcome to our home page')
-    }
-    else if(req.url === '/about'){
-        res.end('Here is our about page')
-    }
-    else{
-        res.end(`<h1>O-OH IDIOT DETECTED</h1><p>IDIOT IDIOT</p>`)
-    }
-})
-server.listen(5000)
-
 const _ = require('lodash')
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
 console.log(newItems)
+console.log('hello pepesad')
